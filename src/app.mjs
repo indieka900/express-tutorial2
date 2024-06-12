ed46dc477fe9d2c47e3a0496f376ce9b0507f756
Joseph <indiekaj@gmail.com>
2024-06-12 23:15:56 +0300

    dotenv

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv'
 const app = express();
 
+dotenv.config()
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
